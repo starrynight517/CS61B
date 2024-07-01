647830fad9b46c5240230bd876ced5c092687aea
JasonZ <1120232523@bit.edu.cn>
2024-07-01 17:31:53 +0800

    completed first part of lab1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -2,15 +2,15 @@
  *  @author YOUR NAME HERE
  */
 public class Collatz {
-/**123
+/*12
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
+        if (n %2 ==0) {
+            return n/2;
+        } else if (n %2==1) {
             return 3 * n + 1;
         } else {
-            return n * 2;
+            return 0;
         }
     }
 
